75c07fbf07777b256fc51b811eb72c793fe28136
Roman Melnychuk <bboyromko@gmail.com>
2023-09-03 15:22:04 +0300

    update api_wrapper

--- API/api_wrapper.py
+++ API/api_wrapper.py
@@ -1,20 +1,26 @@
 import requests
 
 class ApiWrapper:
-    allowed_values = ['activity', 'type', 'participants', 'price', 'link', 'key', 'accessibility']
     URL = 'https://www.boredapi.com/api/activity'
+    def get_response(self, activity=None, type=None, participants=None, price_min=None, price_max=None, accessibility_min=None, accessibility_max=None):
+        params_dict = {
+            'activity':activity,
+            'type': type,
+            'participants': participants,
+            'minprice': price_min,
+            'maxprice': price_max,
+            'minaccessibility': accessibility_min,
+            'maxaccessibility': accessibility_max
+        }
 
-    def get_response(self, **kwargs):
-        result = None
+        params_dict = {key:value for (key,value) in params_dict.items() if value}
+        response = requests.get(self.URL, params=params_dict).json()
 
-        if not kwargs:
-            result = requests.get(self.URL).json()
-        else:
-            difference = set(kwargs) - set(self.allowed_values)
-
-            if difference: result = f'You have passed wrong params -> {difference}'
-            else: result = requests.get(self.URL,params=kwargs).json()
-
-        return result
+        return response
 
 
+
+
+
+
+print(ApiWrapper().get_response())
